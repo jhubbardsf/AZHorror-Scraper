9576f895d17fc471c9861c2078fba3726ce16124
Josh Hubbard <jhubbardsf@gmail.com>
2021-07-31 01:21:20 -0400

    Fixing earlier naming conventioins

--- az_horror_scraper.py
+++ az_horror_scraper.py
@@ -1,6 +1,10 @@
 import requests
+import dateutil
 import time
+import re
 from bs4 import BeautifulSoup
+from dateutil.parser import parse
+
 
 # Needed variables for later
 months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
@@ -25,6 +29,11 @@ for year in years:
             title = elem.select('h1 > strong')
             if (len(title) > 0):
                 movie_title = title[0].text
+                movie_title = re.split('[0-9]{2}/[0-9]{2}/[0-9]{4}', movie_title)[0]
+
+                # Unneeded
+                movie_title = re.split('[0-9]{1}/[0-9]{2}/[0-9]{4}', movie_title)[0]
+                # print(movie_title)
                 movie_titles.append(movie_title)
                 this_month += 1
 
